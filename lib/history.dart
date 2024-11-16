221194348c12bbb29138f42c1041ac0d1696df76
Jeet Ray <jeet.ray@ontariotechu.net>
2024-11-16 03:29:20 -0500

    Switched back from FutureBuilder and implemented database and firebase.

--- lib/history.dart
+++ lib/history.dart
@@ -1,13 +1,156 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class History extends StatefulWidget
-{
-  @override
-  State<StatefulWidget> createState() => historyState();
+class Suggestion {
+  late String displayName;
+  late LatLng coordinates;
+  DocumentReference? reference;
+
+  Suggestion(this.displayName, this.coordinates, {this.reference});
+
+  Suggestion.fromMap(Map<String, dynamic> map) {
+    displayName = map['display_name'] ?? map['displayName'];
+    double latitude;
+    if (map['lat'] == null) {
+      latitude = double.parse(map['lat']);
+    } else if (map['latitude'] is double) {
+      latitude = map['latitude'];
+    } else {
+      latitude = double.parse(map['latitude']);
+    }
+    double longitude;
+    if (map['lon'] == null) {
+      longitude = double.parse(map['lon']);
+    } else if (map['latitude'] is double) {
+      longitude = map['longitude'];
+    } else {
+      longitude = double.parse(map['longitude']);
+    }
+    coordinates = LatLng(latitude, longitude);
+    reference = map['reference'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'displayName': displayName,
+      'latitude': coordinates.latitude.toString(),
+      'longitude': coordinates.longitude.toString(),
+    };
+  }
 }
 
-class historyState extends State<History>
-{
+class SuggestionModel {
+  Database database;
+  FirebaseFirestore firebase = FirebaseFirestore.instance;
+
+  SuggestionModel(this.database);
+
+  Future<void> insertSuggestion(Suggestion suggestion,
+      {String table = 'history'}) async {
+    await database.insert(
+      table,
+      suggestion.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  // Adapted From: https://firebase.google.com/docs/firestore/quickstart#add_data
+  Future<void> insertFireSuggestion(Suggestion suggestion,
+      {String table = 'history'}) async {
+    await firebase
+        .collection(table)
+        .add(suggestion.toMap())
+        .then((DocumentReference doc) => suggestion.reference = doc);
+  }
+
+  Future<void> updateSuggestion(Suggestion suggestion,
+      {String table = 'history'}) async {
+    await database.update(
+      table,
+      suggestion.toMap(),
+      where: "displayName = ?",
+      whereArgs: [suggestion.displayName],
+    );
+  }
+
+  // Adapted From: https://firebase.google.com/docs/firestore/manage-data/add-data#update-data
+  Future<void> updateFireSuggestion(Suggestion suggestion,
+      {String table = 'history'}) async {
+    if (suggestion.reference == null) {
+      await insertFireSuggestion(suggestion);
+    } else {
+      await firebase
+          .collection(table)
+          .doc(suggestion.reference!.id)
+          .update(suggestion.toMap());
+    }
+  }
+
+  Future<void> deleteSuggestionByName(String displayName,
+      {String table = 'history'}) async {
+    await database.delete(
+      table,
+      where: 'displayName = ?',
+      whereArgs: [displayName],
+    );
+  }
+
+  // Adapted From: https://firebase.google.com/docs/firestore/manage-data/delete-data#delete_documents
+  Future<void> deleteFireSuggestionById(String referenceId,
+      {String table = 'history'}) async {
+    await firebase.collection(table).doc(referenceId).delete();
+  }
+
+  // Method to read rows from the 'grades' table [7]
+  Future<List<Suggestion>> getAllSuggestions({String table = 'history'}) async {
+    final List<Map<String, dynamic>> maps = await database.query(table);
+    List<Suggestion> suggestions =
+        maps.map((suggestion) => Suggestion.fromMap(suggestion)).toList();
+    print(suggestions);
+    return suggestions;
+  }
+
+  // Adapted From: https://firebase.google.com/docs/firestore/query-data/get-data#get_all_documents_in_a_collection
+  // And:
+  // Answer: https://stackoverflow.com/a/45200659
+  // User: https://stackoverflow.com/users/5189745/hadrien-lejard
+  Future<List<Suggestion>> getAllFireSuggestions(
+      {String table = 'history'}) async {
+    List<Suggestion> suggestions = [];
+    await firebase.collection(table).get().then((querySnapshot) {
+      suggestions = querySnapshot.docs
+          .map((docSnapshot) => Suggestion.fromMap(
+              {...docSnapshot.data(), "reference": docSnapshot.reference}))
+          .toList();
+    });
+    return suggestions;
+  }
+}
+
+class History extends StatefulWidget {
+  final SuggestionModel? model;
+
+  const History({super.key, required this.title, this.model});
+
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() => historyState(model);
+}
+
+class historyState extends State<History> {
+  late SuggestionModel? model;
+
+  historyState(this.model);
+
+  Future<void> _deleteSuggestion(Suggestion suggestion) async {
+    await model!.deleteSuggestionByName(suggestion.displayName);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,31 +158,92 @@ class historyState extends State<History>
       // Scaffold provides the overall structure for the screen with an AppBar and body
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Text(style: TextStyle(color: Colors.white), "History Page"),  // The title shown in the app's top bar
+        title: Text(
+            style: const TextStyle(color: Colors.white),
+            widget.title), // The title shown in the app's top bar
       ),
-      // Padding widget adds space around the child widgets for better layout
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),  // 20 pixels of padding on all sides
-        // Column widget lays out its children in a vertical direction
-        child: Column(
-          children: <Widget>[
-            Text('History Database To Go Here'),
+      // Adapted From: https://chatgpt.com/share/671e8456-8e90-8000-b9de-afb2cd0d21a4
+      // And:
+      // Answer: https://stackoverflow.com/a/76126936
+      // User: https://stackoverflow.com/users/1817391/aleksey
+      body: model == null
+          ? const Center(child: CircularProgressIndicator())
+          : FutureBuilder<List<Suggestion>>(
+              future: model!.getAllSuggestions(),
+              builder: (BuildContext futureContext,
+                  // Adapted From:
+                  // Answer: https://stackoverflow.com/a/67482488
+                  // User: https://stackoverflow.com/users/6413387/towhid
+                  AsyncSnapshot<List<Suggestion>> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                      child:
+                          CircularProgressIndicator()); // Show loading indicator while fetching data
+                }
 
+                if (snapshot.data!.isEmpty) {
+                  return const Center(
+                      child: Padding(
+                          padding: EdgeInsets.all(70),
+                          child: Text(
+                            'No suggestions found. Add them by searching for locations on the previous screen.',
+                            // Adapted From:
+                            // Answer: https://stackoverflow.com/a/53407050
+                            // User: https://stackoverflow.com/users/5922070/shelly-pritchard
+                            textAlign: TextAlign.center,
+                            // Adapted From: https://api.flutter.dev/flutter/painting/TextStyle-class.html
+                            style: TextStyle(fontSize: 25),
+                          ))); // Display message if no students are found
+                }
 
-            SizedBox(height: 20),  // Adds 20 pixels of vertical space
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (itemContext, index) {
+                    Suggestion suggestion = snapshot.data![index];
+                    return Dismissible(
+                        key: Key(suggestion.displayName),
+                        // Adapted From: https://www.dhiwise.com/post/how-to-implement-flutter-swipe-action-cell-in-mobile-app
+                        background: const Row(children: <Widget>[
+                          Padding(
+                              padding: EdgeInsets.only(left: 12),
+                              child: Icon(Icons.edit, color: Colors.blue)),
+                          Spacer(),
+                        ]),
+                        secondaryBackground: const Row(children: <Widget>[
+                          Spacer(),
+                          Padding(
+                              padding: EdgeInsets.only(right: 12),
+                              child: Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              ))
+                        ]),
 
-            FloatingActionButton(
-              onPressed: () {
-                // pop the screen
-                var value = 'pop';
-                  Navigator.pop(context, value);
+                        // Adapted From: https://docs.flutter.dev/cookbook/gestures/dismissible
+                        // And:
+                        // Answer: https://stackoverflow.com/a/64669848
+                        // User: https://stackoverflow.com/users/8532605/unicornsonlsd
+                        confirmDismiss: (direction) async {
+                          // Adapted From: https://www.dhiwise.com/post/how-to-implement-flutter-swipe-action-cell-in-mobile-app
+                          if (direction == DismissDirection.endToStart) {
+                            await _deleteSuggestion(suggestion);
+                            return true;
+                          }
+                          return false;
+                        },
+                        child: Card(
+                            color: Colors.blue,
+                            child: ListTile(
+                              title: Text(suggestion.displayName,
+                                  style: const TextStyle(color: Colors.white)),
+                              subtitle: Text(
+                                  "Latitude: ${suggestion.coordinates.latitude} | Longitude: ${suggestion.coordinates.longitude}",
+                                  style: const TextStyle(color: Colors.white)),
+                            )));
+                  },
+                );
               },
-              child: Icon(Icons.save),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
-
---
+++ lib/main_future.dart
@@ -0,0 +1,433 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'favourites.dart';
+import 'history.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  // await dotenv.load(fileName: "key.env"); // Ensure dotenv is loaded
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Map Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyMapPage(),
+    );
+  }
+}
+
+// final String _orsApiKey = dotenv.env['ORS_API_KEY'] ?? ''; // Load API key from .env
+final String _orsApiKey =
+    "5b3ce3597851110001cf6248915fcc15b154412d8281d6dd9c531ddf";
+
+class MyMapPage extends StatefulWidget {
+  @override
+  _MyMapPageState createState() => _MyMapPageState();
+}
+
+class _MyMapPageState extends State<MyMapPage> {
+  final MapController _mapController = MapController();
+  final TextEditingController _searchController = TextEditingController();
+  List<Suggestion> _suggestions = [];
+
+  var textStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+  var headerStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.bold);
+
+  LatLng _currentCenter = LatLng(43.9458, -78.8964); // Ontario Tech University
+  // 43.891190, -78.862850
+  LatLng? _destination; // Selected destination coordinates
+  LatLng?
+  _meetup; // Meetup spot halfway between destination and current location.
+  SuggestionModel? _model;
+  int Function(Suggestion, Suggestion)? _comparator;
+
+  @override
+  void initState() {
+    super.initState();
+    _initDatabase(); // Initialize the database when the app starts
+  }
+
+  Future<void> _initDatabase() async {
+    var dbPath = await getDatabasesPath();
+    String path = join(
+        dbPath, 'suggestiondatabase.db');
+
+    _model = SuggestionModel(await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute(
+            "CREATE TABLE history(displayName TEXT PRIMARY KEY, latitude TEXT, longitude TEXT)");
+        await db.execute(
+            "CREATE TABLE favorites(displayName TEXT PRIMARY KEY, latitude TEXT, longitude TEXT)");
+      },
+    ));
+    // TODO
+    // for (Suggestion suggestion in await _model!.getAllSuggestions(comparator: _comparator)) {
+    //   _model!.deleteSuggestionByName(suggestion.displayName);
+    // }
+    setState(() {});
+  }
+
+  _goToHistory(context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => History(title: "History", model: _model)),
+    );
+  }
+
+  _goToFavourites(context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Favourites()),
+    );
+  }
+
+  showOptions(BuildContext context) async {
+    var results =
+    await showDialog(context: context, builder: (context) => showPopup());
+
+    return results;
+  }
+
+  Future<void> _getSuggestions(String input) async {
+    if (input.isEmpty) {
+      setState(() {
+        _suggestions = [];
+      });
+      return;
+    }
+
+    try {
+      final response = await Dio().get(
+        'https://nominatim.openstreetmap.org/search',
+        queryParameters: {
+          'q': input,
+          'format': 'json',
+          'addressdetails': 1,
+          'limit': 5,
+        },
+      );
+
+      setState(() {
+        _suggestions = response.data.map<Suggestion>((suggestion) => Suggestion.fromMap(suggestion)).toList();
+      });
+    } catch (e) {
+      print('Error fetching suggestions: $e');
+    }
+  }
+
+  void _onSuggestionTap(double lat, double lon) {
+    setState(() {
+      _destination = LatLng(lat, lon);
+      _suggestions = [];
+      _searchController.clear();
+    });
+    _mapController.move(_destination!, 13.0);
+  }
+
+  // Calculate the halfway point between two coordinates
+  LatLng calculateHalfwayPoint(LatLng coord1, LatLng coord2) {
+    return LatLng((coord1.latitude + coord2.latitude) / 2,
+        (coord1.longitude + coord2.longitude) / 2);
+  }
+
+  Future<List<LatLng>> _fetchRoute() async {
+    // Fetch the meetup location when a destination is selected
+    // Adapted From: https://ask.openrouteservice.org/t/find-nearest-x/3996
+    LatLng _halfway = calculateHalfwayPoint(_currentCenter, _destination!);
+    List<double> _halfwayCoordinates = [_halfway.longitude, _halfway.latitude];
+    // [-78.88780834814952, 43.917912400000006]
+    // {"type": "Point", "coordinates": [-78.88780834814952, 43.917912400000006]}
+    print(_halfwayCoordinates);
+
+    final meetupUrl = Uri.parse('https://api.openrouteservice.org/pois');
+
+    try {
+      final response = await http.post(meetupUrl,
+          headers: {
+            'Authorization': _orsApiKey,
+            'Accept':
+            'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+            'Content-Type': 'application/json; charset=utf-8'
+            // Adapted From: https://chatgpt.com/share/6737943d-d888-8000-9fa7-a08c34c1d057
+          },
+          body: jsonEncode({
+            'request': 'pois',
+            'geometry': {
+              'buffer': 500,
+              'geojson': {
+                'type': 'Point',
+                'coordinates': _halfwayCoordinates,
+              },
+            },
+            'limit': 2,
+            'sortby': 'distance',
+          }));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        print('Meetup Coordinates Data: $data'); // Debug print
+
+        if (data['features'] != null && data['features'].isNotEmpty) {
+          final coordinates =
+          data['features'][0]['geometry']['coordinates'] as List;
+
+          // setState(() {
+          _meetup = LatLng(coordinates[1], coordinates[0]);
+          // });
+          print("Meetup Coordinates: $_meetup"); // Debug print
+        } else {
+          print("No meetup coordinates in the response.");
+        }
+      } else {
+        print('Failed to load meetup coordinates: Error ${response.statusCode} with response ${response.body}');
+      }
+    } catch (e) {
+      print('Error fetching meetup coordinates: $e');
+    }
+
+    final routeUrl = Uri.parse(
+      // 'https://api.openrouteservice.org/v2/directions/foot-walking?api_key=$_orsApiKey&start=${_currentCenter.longitude},${_currentCenter.latitude}&end=${_destination!.longitude},${_destination!.latitude}',
+      'https://api.openrouteservice.org/v2/directions/driving-car/geojson',
+    );
+
+    try {
+      final response = await http.post(routeUrl,
+          headers: {
+            'Authorization': _orsApiKey,
+            'Accept':
+            'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+            'Content-Type': 'application/json; charset=utf-8'
+          },
+          // Adapted From: https://chatgpt.com/share/6737943d-d888-8000-9fa7-a08c34c1d057
+          // And: https://openrouteservice.org/dev/#/api-docs
+          body: jsonEncode({
+            'coordinates': [
+              [_currentCenter.longitude, _currentCenter.latitude],
+              [_meetup!.longitude, _meetup!.latitude],
+              [_destination!.longitude, _destination!.latitude]
+            ],
+          }));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        print('Route data: $data'); // Debug print
+
+        if (data['features'].isNotEmpty) {
+          final coordinates =
+          data['features'][0]['geometry']['coordinates'] as List;
+
+          // Points for the polyline route
+          List<LatLng> _routePoints = coordinates.map((coord) {
+            return LatLng(coord[1], coord[0]); // Convert to LatLng
+          }).toList();
+          print("Route points: $_routePoints"); // Debug print
+          return _routePoints;
+        } else {
+          print("No route found in the response.");
+          return [];
+        }
+      } else {
+        print('Failed to load route: ${response.statusCode}');
+        return [];
+      }
+    } catch (e) {
+      print('Error fetching route: $e');
+      return [];
+    }
+  }
+
+  Widget mapMaker(BuildContext context, {List<LatLng>? points}) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                    hintText: "Search destination",
+                    border: const OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          var result = showOptions(context);
+                        },
+                        icon: const Icon(Icons.menu))),
+                onChanged: _getSuggestions,
+              ),
+              if (_suggestions.isNotEmpty)
+                Container(
+                  height: 150,
+                  child: ListView.builder(
+                    itemCount: _suggestions.length,
+                    itemBuilder: (context, index) {
+                      final suggestion = _suggestions[index];
+                      return ListTile(
+                        title: Text(suggestion.displayName),
+                        onTap: () => _onSuggestionTap(
+                          suggestion.coordinates.latitude,
+                          suggestion.coordinates.longitude,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: FlutterMap(
+            mapController: _mapController,
+            options: MapOptions(
+              center: _currentCenter,
+              zoom: 13.0,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate:
+                "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c'],
+              ),
+              MarkerLayer(
+                markers: [
+                  Marker(
+                    width: 80.0,
+                    height: 80.0,
+                    point: _currentCenter,
+                    builder: (ctx) => const Icon(
+                      Icons.my_location,
+                      color: Colors.blue,
+                      size: 40.0,
+                    ),
+                  ),
+                  if (_destination != null)
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: _destination!,
+                      builder: (ctx) => const Icon(
+                        Icons.location_pin,
+                        color: Colors.red,
+                        size: 40.0,
+                      ),
+                    ),
+                  if (_destination != null)
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: _meetup!,
+                      builder: (ctx) => const Icon(
+                        Icons.location_pin,
+                        color: Colors.green,
+                        size: 40.0,
+                      ),
+                    ),
+                ],
+              ),
+              PolylineLayer(
+                polylines: [
+                  Polyline(
+                    points: points ?? [],
+                    strokeWidth: 4.0,
+                    color: Colors.blue, // Route color
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Meet Me There'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                _goToFavourites(context);
+              },
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.white,
+              )),
+          IconButton(
+              onPressed: () {
+                _goToHistory(context);
+              },
+              icon: Icon(
+                Icons.history,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: FutureBuilder<List<LatLng>>(
+          future: _fetchRoute(), // Fetch the route when a destination is selected,
+          builder: (BuildContext futureContext,
+              // Adapted From:
+              // Answer: https://stackoverflow.com/a/67482488
+              // User: https://stackoverflow.com/users/6413387/towhid
+              AsyncSnapshot<List<LatLng>> snapshot) {
+            // if (!snapshot.hasData) {
+            //   return const Center(
+            //       child:
+            //           CircularProgressIndicator()); // Show loading indicator while fetching data
+            // }
+
+            if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return mapMaker(context);
+            }
+
+            return mapMaker(context, points: snapshot.data!);
+          }),
+    );
+  }
+}
+
+class showPopup extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => showPopupState();
+}
+
+class showPopupState extends State<showPopup> {
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      title: Text('Saved Locations'),
+      children: [
+        SimpleDialogOption(
+          child: const Text('SHOWS DATABASE OF SAVED LOCATIONS FOR SELECTION'),
+          onPressed: () {
+            Navigator.pop(
+                context, 'WORD'); // Closes the dialog and returns true
+          },
+        ),
+      ],
+    );
+  }
+}
